f39e9cad9c95ff8c08838cf02e1bc56f2a8d3a3a
tatata.probeg <tatata.probeg.git@gmail.com>
2025-03-21 15:29:07 +0900

    Complete tasklist app

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import Rails from "@rails/ujs"
+
+Rails.start();
+window.Rails = Rails; // 追加
+
+console.log(window.Rails); // UJSが動作しているか確認
